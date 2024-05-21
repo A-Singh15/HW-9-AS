812008f3daadb27fc3a4be0d8ba90c436432785e
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-21 13:18:13 -0700

    Update sram_if.sv

--- sram_if.sv
+++ sram_if.sv
@@ -1,17 +1,19 @@
 interface sram_if (
     input logic HCLK
 );
-    logic [20:0] HADDR;
-    logic        HWRITE;
-    logic [7:0]  HWDATA;
-    logic [7:0]  HRDATA;
+    logic [20:0] A;
+    logic        WE_b;
+    logic        CE_b;
+    logic        OE_b;
+    logic [7:0]  DQ;
 
     // Clocking block for SRAM signals
     clocking sram_cb @(posedge HCLK);
-        output HADDR;
-        output HWRITE;
-        output HWDATA;
-        input  HRDATA;
+        output A;
+        output WE_b;
+        output CE_b;
+        output OE_b;
+        inout  DQ;
     endclocking
 
 endinterface
